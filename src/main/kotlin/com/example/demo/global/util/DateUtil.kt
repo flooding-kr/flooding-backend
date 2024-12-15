f2376b81b3fc73fb4928f6ed6d8324168c74bc53
enbraining <me@fodo.dev>
2024-12-15 17:30:25 +0900

    :recycle: 유틸리티 및 써드파티 추가

--- src/main/kotlin/com/example/demo/global/util/DateUtil.kt
+++ src/main/kotlin/com/example/demo/global/util/DateUtil.kt
@@ -1,4 +1,8 @@
 package com.example.demo.global.util
 
-class DateUtil {
-}
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.Date
+
+fun LocalDateTime.toDate(zone: ZoneId = ZoneId.systemDefault()): Date =
+	Date.from(this.atZone(zone).toInstant())
--- src/main/kotlin/com/example/demo/global/util/PasswordUtil.kt
+++ src/main/kotlin/com/example/demo/global/util/PasswordUtil.kt
@@ -1,4 +1,19 @@
 package com.example.demo.global.util
 
+import org.springframework.stereotype.Component
+import java.util.Random
+
+@Component
 class PasswordUtil {
+	fun generateSixRandomCode(): String {
+		val characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+		val random = Random()
+		val code = StringBuilder()
+
+		for (j in 0..5) {
+			code.append(characters[random.nextInt(characters.length)])
+		}
+
+		return code.toString()
+	}
 }
