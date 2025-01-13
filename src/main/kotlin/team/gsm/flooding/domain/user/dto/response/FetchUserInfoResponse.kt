26f216f456d188c498c6b349412bfc883d42b324
enbraining <me@bricn.net>
2025-01-13 22:03:12 +0900

    :recycle: 유저 반환 정보 수정

--- src/main/kotlin/team/gsm/flooding/domain/user/dto/response/FetchUserInfoResponse.kt
+++ src/main/kotlin/team/gsm/flooding/domain/user/dto/response/FetchUserInfoResponse.kt
@@ -1,4 +1,14 @@
 package team.gsm.flooding.domain.user.dto.response
 
-class FetchUserInfoResponse {
-}
+import team.gsm.flooding.domain.user.entity.Gender
+import team.gsm.flooding.domain.user.entity.Role
+import team.gsm.flooding.domain.user.entity.StudentInfo
+
+data class FetchUserInfoResponse (
+	val name: String,
+	val email: String,
+	val isVerified: Boolean,
+	val studentInfo: StudentInfoResponse,
+	val gender: Gender,
+	val roles: List<Role>
+)
--- src/main/kotlin/team/gsm/flooding/domain/user/dto/response/StudentInfoResponse.kt
+++ src/main/kotlin/team/gsm/flooding/domain/user/dto/response/StudentInfoResponse.kt
@@ -1,4 +1,9 @@
 package team.gsm.flooding.domain.user.dto.response
 
-class StudentInfoResponse {
-}
+data class StudentInfoResponse (
+	val isGraduate: Boolean,
+	val grade: Int,
+	val classroom: Int,
+	val number: Int,
+	val year: Int,
+)
