2b930dfdaffca55d906d41bbfffc2da3f40859f6
enbraining <me@bricn.net>
2025-02-02 21:37:31 +0900

    :recycle: 미완성 로직 제거

--- src/main/kotlin/team/gsm/flooding/domain/attendance/controller/AttendanceController.kt
+++ src/main/kotlin/team/gsm/flooding/domain/attendance/controller/AttendanceController.kt
@@ -8,35 +8,16 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.gsm.flooding.domain.attendance.dto.request.ReserveHomebaseTableRequest
-import team.gsm.flooding.domain.attendance.dto.request.UpdateHomebaseTableRequest
-import team.gsm.flooding.domain.attendance.usecase.CancelHomebaseTableUsecase
 import team.gsm.flooding.domain.attendance.usecase.ReserveHomebaseTableUsecase
-import team.gsm.flooding.domain.attendance.usecase.UpdateHomebaseTableUsecase
 
 @RestController
 @RequestMapping("attendance")
 class AttendanceController (
 	private val reserveHomebaseTableUsecase: ReserveHomebaseTableUsecase,
-	private val updateHomebaseTableUsecase: UpdateHomebaseTableUsecase,
-	private val cancelHomebaseTableUsecase: CancelHomebaseTableUsecase
 ) {
 	@PostMapping("homebase")
 	fun reserveHomebaseTable(@RequestBody request: ReserveHomebaseTableRequest): ResponseEntity<Unit> {
 		return reserveHomebaseTableUsecase.execute(request).run {
-			ResponseEntity.ok().build()
-		}
-	}
-
-	@PatchMapping("homebase")
-	fun updateHomebaseTable(@RequestBody request: UpdateHomebaseTableRequest): ResponseEntity<Unit> {
-		return updateHomebaseTableUsecase.execute(request).run {
-			ResponseEntity.ok().build()
-		}
-	}
-
-	@DeleteMapping("homebase")
-	fun cancelHomebaseTable(): ResponseEntity<Unit> {
-		return cancelHomebaseTableUsecase.execute().run {
 			ResponseEntity.ok().build()
 		}
 	}
--- src/main/kotlin/team/gsm/flooding/domain/attendance/dto/request/UpdateHomebaseTableRequest.kt
+++
@@ -1,7 +0,0 @@
-package team.gsm.flooding.domain.attendance.dto.request
-
-import java.util.UUID
-
-class UpdateHomebaseTableRequest (
-	val participants: MutableList<UUID>,
-)
