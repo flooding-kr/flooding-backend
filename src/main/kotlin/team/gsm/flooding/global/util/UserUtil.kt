456665c6cbeb0c5bd8349a36eb1bb5fa113d051b
이현준 <11dlguswns@naver.com>
2025-02-08 02:33:54 +0900

    ✨ 유저로 학번을 구하는 Util 메서드

--- src/main/kotlin/team/gsm/flooding/global/util/UserUtil.kt
+++ src/main/kotlin/team/gsm/flooding/global/util/UserUtil.kt
@@ -6,11 +6,22 @@ import team.gsm.flooding.global.exception.ExceptionEnum
 import team.gsm.flooding.global.exception.ExpectedException
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
+import java.time.LocalDate
 
 @Component
 class UserUtil (
 	private val userRepository: UserRepository
 ) {
+	companion object {
+		fun getSchoolNumber(user: User): String {
+			val studentInfo = user.studentInfo
+			val nowDateYear = LocalDate.now().year
+			val classGrade = (nowDateYear - 2015 - studentInfo.year).toString()
+
+			return classGrade + studentInfo.classroom + studentInfo.number
+		}
+	}
+
 	fun getUser(): User {
 		val email = SecurityContextHolder.getContext().authentication.name
 		return userRepository.findByEmail(email).orElseThrow {
