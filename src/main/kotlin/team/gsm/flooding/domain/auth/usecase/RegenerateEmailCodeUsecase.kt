9642a8be40c1bce283c9e9bc681866d5bb9f3489
enbraining <me@bricn.net>
2025-02-16 15:02:10 +0900

    :recycle: 저장된 이메일 인증코드가 없어도 발급할 수 있도록 변경

--- src/main/kotlin/team/gsm/flooding/domain/auth/usecase/RegenerateEmailCodeUsecase.kt
+++ src/main/kotlin/team/gsm/flooding/domain/auth/usecase/RegenerateEmailCodeUsecase.kt
@@ -2,6 +2,7 @@ package team.gsm.flooding.domain.auth.usecase
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import team.gsm.flooding.domain.auth.entity.VerifyCode
 import team.gsm.flooding.domain.auth.repository.VerifyCodeRepository
 import team.gsm.flooding.domain.user.repository.UserRepository
 import team.gsm.flooding.global.exception.ExceptionEnum
@@ -22,19 +23,18 @@ class RegenerateEmailCodeUsecase(
 			userRepository.findByEmail(email).orElseThrow {
 				ExpectedException(ExceptionEnum.NOT_FOUND_USER)
 			}
-		val id = userByEmail.id
-		requireNotNull(id) { "id cannot be null" }
+		val id = requireNotNull(userByEmail.id)
 
 		if (userByEmail.isVerified) {
 			throw ExpectedException(ExceptionEnum.ALREADY_VERIFY_EMAIL)
 		}
 
-		val verifyCodeEntity =
-			verifyCodeRepository.findById(id).orElseThrow {
-				ExpectedException(ExceptionEnum.NOT_FOUND_VERIFY_CODE)
-			}
-
 		val newVerifyCode = passwordUtil.generateSixRandomCode()
+		val verifyCodeEntity =
+			verifyCodeRepository.findById(id).orElse(
+				VerifyCode(id, newVerifyCode, 15),
+			)
+
 		emailAdapter.sendVerifyCode(email, newVerifyCode)
 
 		verifyCodeRepository.save(verifyCodeEntity.copy(code = newVerifyCode))
