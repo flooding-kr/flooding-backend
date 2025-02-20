ebc24c7543cbe2eec9d01ab039180e3dc156afb6
이현준 <11dlguswns@naver.com>
2025-02-20 17:40:48 +0900

    ✨ 구성원 추방하기 기능

--- src/main/kotlin/team/gsm/flooding/domain/club/controller/ClubController.kt
+++ src/main/kotlin/team/gsm/flooding/domain/club/controller/ClubController.kt
@@ -2,7 +2,9 @@ package team.gsm.flooding.domain.club.controller
 
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,12 +15,15 @@ import team.gsm.flooding.domain.club.dto.response.FindClubFilterResponse
 import team.gsm.flooding.domain.club.entity.ClubType
 import team.gsm.flooding.domain.club.usecase.CreateClubUsecase
 import team.gsm.flooding.domain.club.usecase.FindClubFilterUsecase
+import team.gsm.flooding.domain.club.usecase.RemoveClubMemberUsecase
+import java.util.UUID
 
 @RestController
 @RequestMapping("club")
 class ClubController(
 	private val createClubUsecase: CreateClubUsecase,
 	private val findClubFilterUsecase: FindClubFilterUsecase,
+	private val removeClubMemberUsecase: RemoveClubMemberUsecase,
 ) {
 	@PostMapping
 	fun createClub(
@@ -34,5 +39,14 @@ class ClubController(
 	): ResponseEntity<FindClubFilterResponse> =
 		findClubFilterUsecase.execute(type).run {
 			ResponseEntity.ok(this)
+		}
+
+	@DeleteMapping("{clubId}/member/{userId}")
+	fun removeClubMember(
+		@PathVariable clubId: UUID,
+		@PathVariable userId: UUID,
+	): ResponseEntity<Unit> =
+		removeClubMemberUsecase.execute(clubId, userId).run {
+			ResponseEntity.ok().build()
 		}
 }
