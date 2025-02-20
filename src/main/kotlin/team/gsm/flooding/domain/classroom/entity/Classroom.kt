bea4f5e6664035052f559ac6ba3f2b32581bb6d0
enbraining <me@bricn.net>
2025-02-20 22:55:19 +0900

    ♻️ 열거형 문자열로 저장

--- src/main/kotlin/team/gsm/flooding/domain/classroom/entity/Classroom.kt
+++ src/main/kotlin/team/gsm/flooding/domain/classroom/entity/Classroom.kt
@@ -2,6 +2,8 @@ package team.gsm.flooding.domain.classroom.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -23,6 +25,9 @@ data class Classroom(
 	val description: String,
 
 	val isHomebase: Boolean,
+
+	@Enumerated(EnumType.STRING)
+	val buildingType: BuildingType,
 
 	@ManyToOne
 	val teacher: User,
