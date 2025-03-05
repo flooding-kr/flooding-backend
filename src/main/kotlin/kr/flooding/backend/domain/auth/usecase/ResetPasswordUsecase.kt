ffe808912cbf40b7c982320bdd367afaccdefe37
enbraining <me@bricn.net>
2025-03-05 14:46:58 +0900

    :recycle: 비밀번호 암호화 추가

--- src/main/kotlin/kr/flooding/backend/domain/auth/usecase/ResetPasswordUsecase.kt
+++ src/main/kotlin/kr/flooding/backend/domain/auth/usecase/ResetPasswordUsecase.kt
@@ -4,7 +4,9 @@ import kr.flooding.backend.domain.auth.dto.request.ResetPasswordRequest
 import kr.flooding.backend.domain.user.repository.UserRepository
 import kr.flooding.backend.global.exception.ExceptionEnum
 import kr.flooding.backend.global.exception.HttpException
+import kr.flooding.backend.global.exception.toPair
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -13,22 +15,24 @@ import org.springframework.transaction.annotation.Transactional
 class ResetPasswordUsecase(
 	private val redisTemplate: RedisTemplate<String, String>,
 	private val userRepository: UserRepository,
+	private val passwordEncoder: PasswordEncoder,
 ) {
 	fun execute(request: ResetPasswordRequest) {
 		val userByEmail =
 			userRepository.findByEmail(request.email).orElseThrow {
-				HttpException(ExceptionEnum.NOT_FOUND_USER)
+				HttpException(ExceptionEnum.USER.NOT_FOUND_USER.toPair())
 			}
 		val userId = requireNotNull(userByEmail.id)
 
 		val key = "reset-password-code:$userId"
 		val requestList = redisTemplate.opsForList().range(key, 0, -1)
 		if (requestList.isNullOrEmpty() || !requestList.contains(request.code)) {
-			throw HttpException(ExceptionEnum.NOT_FOUND_RESET_PASSWORD_REQUEST_CODE)
+			throw HttpException(ExceptionEnum.AUTH.NOT_FOUND_RESET_PASSWORD_REQUEST_CODE.toPair())
 		}
 
 		redisTemplate.delete(key)
 
-		userByEmail.resetPassword(request.password)
+		val encodedPassword = passwordEncoder.encode(request.password)
+		userByEmail.resetPassword(encodedPassword)
 	}
 }
