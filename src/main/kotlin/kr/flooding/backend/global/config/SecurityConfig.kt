7e2b27eb415d7aa1fcffddad4f457a037ce7d77d
skgur07 <pigjaoki0970@gmail.com>
2025-03-05 14:54:10 +0900

    security 설정

--- src/main/kotlin/kr/flooding/backend/global/config/SecurityConfig.kt
+++ src/main/kotlin/kr/flooding/backend/global/config/SecurityConfig.kt
@@ -85,6 +85,8 @@ class SecurityConfig(
 					.hasAnyAuthority(Role.ROLE_USER.name)
 					.requestMatchers(HttpMethod.GET, "/club/{clubId}")
 					.hasAnyAuthority(Role.ROLE_USER.name)
+					.requestMatchers(HttpMethod.POST, "/club/form")
+					.hasAnyAuthority(Role.ROLE_USER.name)
 					// 파일
 					.requestMatchers(HttpMethod.POST, "/file/image")
 					.hasAnyAuthority(Role.ROLE_USER.name)
