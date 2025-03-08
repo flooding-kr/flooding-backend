2e02b770378818f0f4536caa61b43c9db0cf68e8
이현준 <11dlguswns@naver.com>
2025-03-08 21:11:22 +0900

    ♻️ 홈베이스 조회시 날짜 반환 제거

--- src/main/kotlin/kr/flooding/backend/domain/homebase/dto/response/FetchMyReservedHomebaseResponse.kt
+++ src/main/kotlin/kr/flooding/backend/domain/homebase/dto/response/FetchMyReservedHomebaseResponse.kt
@@ -2,13 +2,11 @@ package kr.flooding.backend.domain.homebase.dto.response
 
 import kr.flooding.backend.domain.homebase.entity.HomebaseGroup
 import kr.flooding.backend.domain.user.entity.User
-import java.time.LocalDate
 import java.util.UUID
 
 class FetchMyReservedHomebaseResponse(
 	val homebaseGroupId: UUID?,
 	val homebaseTable: HomebaseTableResponse,
-	val attendedAt: LocalDate?,
 	val period: Int,
 	val isProposer: Boolean,
 	val participants: List<kr.flooding.backend.domain.homebase.dto.response.HomebaseParticipantResponse>,
@@ -23,7 +21,6 @@ class FetchMyReservedHomebaseResponse(
 			return FetchMyReservedHomebaseResponse(
 				homebaseGroupId = homebaseGroup.id,
 				homebaseTable = HomebaseTableResponse.toDto(homebaseGroup.homebaseTable),
-				attendedAt = homebaseGroup.attendedAt,
 				period = homebaseGroup.period,
 				participants =
 					listOf(proposerAsHomebaseParticipant) +
