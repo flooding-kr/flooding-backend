a44d723f9393a78cb64da2caf3bd0efdefaba13a
enbraining <me@bricn.net>
2025-03-11 16:06:10 +0900

    :recycle: Dockerfile

---
+++ src/main/kotlin/kr/flooding/backend/global/thirdparty/mail/MailConfig.kt
@@ -0,0 +1,42 @@
+package kr.flooding.backend.global.thirdparty.email
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.mail.MailSender
+import org.springframework.mail.javamail.JavaMailSenderImpl
+import java.util.Properties
+
+@Configuration
+class MailConfig(
+	@Value("\${spring.mail.host}")
+	private val host: String,
+	@Value("\${spring.mail.username}")
+	private val username: String,
+	@Value("\${spring.mail.password}")
+	private val password: String,
+) {
+	@Bean
+	fun mailSender(): MailSender {
+		val javaMailSender = JavaMailSenderImpl()
+		javaMailSender.host = host
+		javaMailSender.username = username
+		javaMailSender.password = password
+
+		javaMailSender.port = 587
+		javaMailSender.javaMailProperties = getMailProperties()
+
+		return javaMailSender
+	}
+
+	private fun getMailProperties(): Properties {
+		val properties = Properties()
+		properties.setProperty("mail.transport.protocol", "smtp")
+		properties.setProperty("mail.smtp.auth", "true")
+		properties.setProperty("mail.smtp.starttls.enable", "true")
+		properties.setProperty("mail.debug", "true")
+		properties.setProperty("mail.smtp.ssl.trust", host)
+		properties.setProperty("mail.smtp.ssl.enable", "true")
+		return properties
+	}
+}
