a41f7779a8a739f1a2c154f1c79aa18a99f1d1c7
enbraining <me@bricn.net>
2025-03-25 14:03:22 +0900

    :recycle: 시간대 Asia/Seoul로 변경

--- src/main/kotlin/kr/flooding/backend/FloodingApplication.kt
+++ src/main/kotlin/kr/flooding/backend/FloodingApplication.kt
@@ -2,10 +2,12 @@ package kr.flooding.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class FloodingApplication
 
 fun main(args: Array<String>) {
+	TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
 	runApplication<FloodingApplication>(*args)
 }
