5c3b2df455f900cec262b0e393f06c2502dff9e0
enbraining <me@bricn.net>
2025-04-03 20:23:02 +0900

    :recycle: Ktlint 문법 규칙 반영

--- src/main/kotlin/kr/flooding/backend/domain/clubApplicant/repository/jdsl/ClubApplicantJdslRepository.kt
+++ src/main/kotlin/kr/flooding/backend/domain/clubApplicant/repository/jdsl/ClubApplicantJdslRepository.kt
@@ -3,7 +3,8 @@ package kr.flooding.backend.domain.clubApplicant.repository.jdsl
 import kr.flooding.backend.domain.club.entity.Club
 import kr.flooding.backend.domain.clubApplicant.domain.ClubApplicant
 import kr.flooding.backend.domain.user.entity.User
-import java.util.*
+import java.util.Optional
+import java.util.UUID
 
 interface ClubApplicantJdslRepository {
 	fun findWithClubAndUserByClub(club: Club): List<ClubApplicant>
