3662a68bed8f45c38ebeca1cc1278447c6e120fc
enbraining <me@bricn.net>
2025-04-09 20:43:19 +0900

    :recycle: 동아리 테이블, 선생님 추가

--- src/main/kotlin/kr/flooding/backend/domain/club/dto/response/FetchClubResponse.kt
+++ src/main/kotlin/kr/flooding/backend/domain/club/dto/response/FetchClubResponse.kt
@@ -1,9 +1,9 @@
 package kr.flooding.backend.domain.club.dto.response
 
-import kr.flooding.backend.domain.classroom.entity.Classroom
 import kr.flooding.backend.domain.club.entity.Club
 import kr.flooding.backend.domain.club.entity.ClubStatus
 import kr.flooding.backend.domain.club.entity.ClubType
+import kr.flooding.backend.domain.club.model.ClubClassroomModel
 import kr.flooding.backend.domain.clubMember.entity.ClubMember
 import java.util.UUID
 
@@ -11,28 +11,37 @@ class FetchClubResponse(
 	val id: UUID?,
 	val name: String,
 	val description: String,
-	val classroom: Classroom,
+	val classroom: ClubClassroomModel,
 	val activityImageUrls: List<String>,
+	val thumbnailImageUrl: String?,
 	val type: ClubType,
 	val isRecruiting: Boolean,
 	val clubMembers: List<ClubMemberResponse>,
 	val status: ClubStatus,
+	val leader: ClubMemberResponse,
+	val teacher: ClubMemberResponse?,
 ) {
 	companion object {
 		fun toDto(
 			club: Club,
 			clubMembers: List<ClubMember>,
-		): FetchClubResponse =
-			FetchClubResponse(
+		): FetchClubResponse {
+			val teacher = if (club.teacher != null) ClubMemberResponse.toDto(club.teacher!!) else null
+
+			return FetchClubResponse(
 				id = club.id,
 				name = club.name,
 				description = club.description,
-				classroom = club.classroom,
+				classroom = ClubClassroomModel.toDto(club.classroom),
 				activityImageUrls = club.activityImageUrls,
 				status = club.status,
 				type = club.type,
 				isRecruiting = club.isRecruiting,
 				clubMembers = clubMembers.map { ClubMemberResponse.toDto(it.user) },
+				thumbnailImageUrl = club.thumbnailImageUrl,
+				leader = ClubMemberResponse.toDto(club.leader),
+				teacher = teacher,
 			)
+		}
 	}
 }
