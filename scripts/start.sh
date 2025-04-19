bdc13eaa39683e6e99990aaccb91e56acaf21ca5
이성민 <l_keon@iseongmin-ui-MacBookPro.local>
2025-04-20 00:20:34 +0900

    update start

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo docker run -d -p 8080:8080 --name flooding-server --env-file .env 588738598492.dkr.ecr.ap-northeast-2.amazonaws.com/flooding-ecr:latest
+
+sudo docker run -d -p 8080:8080 --name flooding-server \
+  --env-file /home/ubuntu/.env \
+  588738598492.dkr.ecr.ap-northeast-2.amazonaws.com/flooding-ecr:latest
