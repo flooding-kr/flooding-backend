6531a1ff78000a5f281224ac5929672790d8cfdd
이성민 <l_keon@iseongmin-ui-MacBookPro.local>
2025-04-19 23:11:59 +0900

    update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 aws ssm get-parameter --name "/flooding/prod/env" --with-decryption --query "Parameter.Value" --output text > /home/ubuntu/.env
+
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 588738598492.dkr.ecr.ap-northeast-2.amazonaws.com
+
+docker pull 588738598492.dkr.ecr.ap-northeast-2.amazonaws.com/flooding-ecr:latest
