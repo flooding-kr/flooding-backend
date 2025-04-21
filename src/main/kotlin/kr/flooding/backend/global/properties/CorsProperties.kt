1e20eb10478654c3481f7b61e39c57478aa0e6c7
enbraining <me@bricn.net>
2025-04-21 09:13:38 +0900

    :recycle: Properties 빈 직접 등록

--- src/main/kotlin/kr/flooding/backend/global/properties/CorsProperties.kt
+++ src/main/kotlin/kr/flooding/backend/global/properties/CorsProperties.kt
@@ -1,11 +1,9 @@
 package kr.flooding.backend.global.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
 
-@Configuration
 @ConfigurationProperties(prefix = "cors")
-class CorsProperties {
-    lateinit var allowedOrigins: List<String>
-}
+class CorsProperties (
+    var allowedOrigins: List<String>
+)
 
--- src/main/kotlin/kr/flooding/backend/global/properties/JwtProperties.kt
+++ src/main/kotlin/kr/flooding/backend/global/properties/JwtProperties.kt
@@ -3,12 +3,11 @@ package kr.flooding.backend.global.properties
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
-@Configuration
 @ConfigurationProperties(prefix = "jwt")
-class JwtProperties {
-    lateinit var accessTokenKey: String
-    var accessTokenExpires: Long = 0
-    lateinit var refreshTokenKey: String
-    var refreshTokenExpires: Long = 0
-}
+class JwtProperties (
+    val accessTokenKey: String,
+    val accessTokenExpires: Long,
+    val refreshTokenKey: String,
+    val refreshTokenExpires: Long
+)
 
