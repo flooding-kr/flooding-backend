a39ab1f9484af27e2de0219117770bc02a96eecc
skgur07 <pigjaoki0970@gmail.com>
2025-04-29 17:13:52 +0900

    dto null 제거

--- src/main/kotlin/kr/flooding/backend/domain/selfStudy/dto/request/FetchSelfStudyRequest.kt
+++ src/main/kotlin/kr/flooding/backend/domain/selfStudy/dto/request/FetchSelfStudyRequest.kt
@@ -3,8 +3,8 @@ package kr.flooding.backend.domain.selfStudy.dto.request
 import kr.flooding.backend.domain.user.enums.Gender
 
 class FetchSelfStudyRequest(
-    val name: String? = null,
-    val grade: Int? = null,
-    val classroom: Int? = null,
-    val gender: Gender? = null,
+    val name: String?,
+    val grade: Int?,
+    val classroom: Int?,
+    val gender: Gender?,
 )
