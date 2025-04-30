46d64101ffcaf5f8adc12be82cbbfb05d40e4afd
이현준 <11dlguswns@naver.com>
2025-04-30 12:30:06 +0900

    ♻️ 자습 정지 JPA PK Long타입으로 수정

--- src/main/kotlin/kr/flooding/backend/domain/selfStudy/persistence/repository/jpa/SelfStudySuspensionJpaRepository.kt
+++ src/main/kotlin/kr/flooding/backend/domain/selfStudy/persistence/repository/jpa/SelfStudySuspensionJpaRepository.kt
@@ -4,9 +4,8 @@ import kr.flooding.backend.domain.selfStudy.persistence.entity.SelfStudySuspensi
 import kr.flooding.backend.domain.user.persistence.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import java.time.LocalDate
-import java.util.UUID
 
-interface SelfStudySuspensionJpaRepository: JpaRepository<SelfStudySuspension, UUID> {
+interface SelfStudySuspensionJpaRepository: JpaRepository<SelfStudySuspension, Long> {
     fun existsByStudent(student: User): Boolean
     fun deleteAllByResumeDateBefore(currentDate: LocalDate)
 }
