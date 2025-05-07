9b39cfd3984895fb669e6dd3d0d8efde3e40f3f1
이현준 <11dlguswns@naver.com>
2025-05-08 07:49:34 +0900

    ♻️ department 컬럼 Enum String으로 저장

--- src/main/kotlin/kr/flooding/backend/domain/user/persistence/entity/TeacherInfo.kt
+++ src/main/kotlin/kr/flooding/backend/domain/user/persistence/entity/TeacherInfo.kt
@@ -1,9 +1,12 @@
 package kr.flooding.backend.domain.user.persistence.entity
 
 import jakarta.persistence.Embeddable
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import kr.flooding.backend.domain.user.enums.Department
 
 @Embeddable
 data class TeacherInfo(
+	@Enumerated(EnumType.STRING)
 	val department: Department,
 )
