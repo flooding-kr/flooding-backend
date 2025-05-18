c85a86decd52cb12d8bf6b26b1d648ffef58f55d
enbraining <me@bricn.net>
2025-05-18 17:04:40 +0900

    :recycle: 주석 추가

--- src/main/kotlin/kr/flooding/backend/global/util/DateUtil.kt
+++ src/main/kotlin/kr/flooding/backend/global/util/DateUtil.kt
@@ -8,6 +8,8 @@ import java.util.Date
 class DateUtil {
 	companion object {
 		fun LocalDateTime.toDate(zone: ZoneId = ZoneId.systemDefault()): Date = Date.from(this.atZone(zone).toInstant())
+
+		// Following LocalDate's atStartOfDay() naming
 		fun LocalDate.atEndOfDay(): LocalDateTime = this.atStartOfDay().plusDays(1)
 	}
 }
