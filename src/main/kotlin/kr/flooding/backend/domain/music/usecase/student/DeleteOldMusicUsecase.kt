f4a0b79a07d05e58173d88ee951be9b9e54433ea
이현준 <11dlguswns@naver.com>
2025-05-20 20:47:37 +0900

    ♻️ 오래된 음악 삭제 로직 트랜잭션 추가

--- src/main/kotlin/kr/flooding/backend/domain/music/usecase/student/DeleteOldMusicUsecase.kt
+++ src/main/kotlin/kr/flooding/backend/domain/music/usecase/student/DeleteOldMusicUsecase.kt
@@ -1,11 +1,13 @@
 package kr.flooding.backend.domain.music.usecase.student
 
+import jakarta.transaction.Transactional
 import kr.flooding.backend.domain.music.persistence.repository.jpa.MusicJpaRepository
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 import java.time.LocalTime
 
 @Service
+@Transactional
 class DeleteOldMusicUsecase(
     private val musicJpaRepository: MusicJpaRepository,
 ) {
